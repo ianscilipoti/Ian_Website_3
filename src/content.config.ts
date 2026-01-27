063770da2625933ec57dd4a29a65666e67f77a3c
ianscilipoti <ianscilipoti@gmail.com>
2026-01-27 14:21:38 -0500

    Improved content, switched to mdx for improve styling on project pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,17 +2,18 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const projects = defineCollection({
-  loader: glob({ pattern: '**/index.md', base: './src/content/projects' }),
+  loader: glob({ pattern: '**/index.mdx', base: './src/content/projects' }),
   schema: ({ image }) => z.object({
     projectId: z.string(),
     title: z.string(),
     description: z.string().optional(),
     previewImg: image().optional(),
     skills: z.array(z.string()).optional(),
-    order: z.number().optional(),
     color: z.enum(['purple', 'green', 'blue', 'orange']).optional(),
     date: z.number().optional()
   }),
 });
+
+console.log(projects);
 
 export const collections = { projects };
